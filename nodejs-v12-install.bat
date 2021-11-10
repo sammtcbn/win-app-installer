2fcee47d34333bf5855bab2f8b0149a2d9a335a2
sammtcbn <sammtcbn@gmail.com>
2021-11-11 02:01:37 +0800

    install nodejs v12

---
+++ nodejs-v12-install.bat
@@ -0,0 +1,10 @@
+@echo off
+set currpath=%~dp0
+if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
+cd /d "%currpath%"
+for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
+
+set ver=12.22.7
+set fn=node-v%ver%-x64.msi
+
+%archivespath%\%fn% /quiet
