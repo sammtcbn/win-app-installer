ba31ffaea40e5cb7bbc57a643ad86454f29d7cc9
sammtcbn <sammtcbn@gmail.com>
2021-11-10 18:37:23 +0800

    install winmerge

---
+++ winmerge-download.bat
@@ -0,0 +1,11 @@
+@echo off
+set currpath=%~dp0
+if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
+cd /d "%currpath%"
+for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
+
+set ver=2.16.14
+set fn=WinMerge-%ver%-x64-Setup.exe
+set url=https://github.com/WinMerge/winmerge/releases/download/v%ver%/%fn%
+
+call utils\curl-dl.bat "%url%" %fn%
