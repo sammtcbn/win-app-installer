cd8c22ba4744a1c7860a9535efd2bc574c3a377a
sammtcbn <sammtcbn@gmail.com>
2021-11-11 01:10:34 +0800

    install NotePad++

---
+++ npp-download.bat
@@ -0,0 +1,11 @@
+@echo off
+set currpath=%~dp0
+if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
+cd /d "%currpath%"
+for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
+
+set ver=8.1.9
+set fn=npp.%ver%.Installer.x64.exe
+set url=https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v%ver%/%fn%
+
+call utils\curl-dl.bat "%url%" %fn%
