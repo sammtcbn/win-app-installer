9aeb3a5fa293a0c9632d807b6e5e98a9d983558f
sammtcbn <sammtcbn@gmail.com>
2021-11-11 01:14:46 +0800

    install 7-Zip

---
+++ 7zip-install.bat
@@ -0,0 +1,10 @@
+@echo off
+set currpath=%~dp0
+if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
+cd /d "%currpath%"
+for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
+
+set ver=1900
+set fn=7z%ver%-x64.exe
+
+%archivespath%\%fn% /S
