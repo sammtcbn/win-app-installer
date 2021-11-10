9a3fa35c2e07fedb7d9f5e24a6b6bed0f70d2784
sammtcbn <sammtcbn@gmail.com>
2021-11-10 17:45:09 +0800

    install grafana

---
+++ grafana-download.bat
@@ -0,0 +1,11 @@
+@echo off
+set currpath=%~dp0
+if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
+cd /d "%currpath%"
+for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
+
+set ver=8.1.5
+set fn=grafana-%ver%.windows-amd64.msi
+set url=https://dl.grafana.com/oss/release/%fn%
+
+call utils\curl-dl.bat "%url%" %fn%
---
+++ grafana-install.bat
@@ -0,0 +1,12 @@
+@echo off
+set currpath=%~dp0
+if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
+cd /d "%currpath%"
+for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
+
+set ver=8.1.5
+set fn=grafana-%ver%.windows-amd64.msi
+
+%archivespath%\%fn% /quiet /qn /norestart
+
+copy /Y Grafana.html C:\Users\%UserName%\Desktop
