95ff51572b019b3c3997e3deb6777b520540c5c3
sammtcbn <sammtcbn@gmail.com>
2021-11-11 10:50:50 +0800

    install golang

---
+++ go-download.bat
@@ -0,0 +1,11 @@
+@echo off
+set currpath=%~dp0
+if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
+cd /d "%currpath%"
+for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
+
+set ver=1.17.1
+set fn=go1.17.1.windows-amd64.msi
+set url=https://golang.org/dl/%fn%
+
+call utils\curl-dl.bat "%url%" %fn%
