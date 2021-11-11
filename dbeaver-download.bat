784df37b72e3853901439b6621feef89a7211056
sammtcbn <sammtcbn@gmail.com>
2021-11-11 12:36:13 +0800

    DBeaver change to 21.2.4

--- dbeaver-download.bat
+++ dbeaver-download.bat
@@ -4,7 +4,7 @@ if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
 cd /d "%currpath%"
 for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
 
-set ver=21.0.0
+set ver=21.2.4
 set fn=dbeaver-ce-%ver%-x86_64-setup.exe
 set url=https://github.com/dbeaver/dbeaver/releases/download/%ver%/%fn%
 
