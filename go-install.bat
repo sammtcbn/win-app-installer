ca826075bdf64e3bdfef02f9f6c1d0da6f7edf9c
sammtcbn <sammtcbn@gmail.com>
2021-11-11 11:01:23 +0800

    fix script for install golang

--- go-install.bat
+++ go-install.bat
@@ -5,6 +5,6 @@ cd /d "%currpath%"
 for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
 
 set ver=1.17.1
-set fn=go1.17.1.windows-amd64.msi
+set fn=go%ver%.windows-amd64.msi
 
 %archivespath%\%fn% /quiet /qn /norestart
