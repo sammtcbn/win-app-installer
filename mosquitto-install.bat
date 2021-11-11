b65d6e6061c57be8d61c5c6494f4f324b72de962
sammtcbn <sammtcbn@gmail.com>
2021-11-11 11:34:34 +0800

    install mosquitto

---
+++ mosquitto-install.bat
@@ -0,0 +1,12 @@
+@echo off
+set currpath=%~dp0
+if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
+cd /d "%currpath%"
+for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
+
+set ver=2.0.12
+set fn=mosquitto-%ver%-install-windows-x64.exe
+
+%archivespath%\%fn% /S
+
+net start mosquitto
