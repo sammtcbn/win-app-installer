db3e832ff23f7fdf7779e2e70f29a8fbb15da5e9
sammtcbn <sammtcbn@gmail.com>
2021-11-11 11:01:34 +0800

    winstall mqttfx

---
+++ mqttfx-download.bat
@@ -0,0 +1,11 @@
+@echo off
+set currpath=%~dp0
+if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
+cd /d "%currpath%"
+for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
+
+set ver=1.7.1
+set fn=mqttfx-%ver%-windows-x64.exe
+set url=http://jensd.de/apps/mqttfx/%ver%/%fn%
+
+call utils\curl-dl.bat "%url%" %fn%
---
+++ mqttfx-install.bat
@@ -0,0 +1,10 @@
+@echo off
+set currpath=%~dp0
+if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
+cd /d "%currpath%"
+for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
+
+set ver=1.7.1
+set fn=mqttfx-%ver%-windows-x64.exe
+
+%archivespath%\%fn% -q
