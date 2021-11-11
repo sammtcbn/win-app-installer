61a61b4a98ef4567fbe149d570454cff1fb2df57
sammtcbn <sammtcbn@gmail.com>
2021-11-11 12:34:18 +0800

    install DBeaver

---
+++ dbeaver-install.bat
@@ -0,0 +1,10 @@
+@echo off
+set currpath=%~dp0
+if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
+cd /d "%currpath%"
+for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
+
+set ver=21.0.0
+set fn=dbeaver-ce-%ver%-x86_64-setup.exe
+
+%archivespath%\%fn% /S /allusers
