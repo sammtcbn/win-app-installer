2cd0a4573f5eceb4f0c99894f1c139a2049575f6
sammtcbn <sammtcbn@gmail.com>
2021-11-11 12:38:58 +0800

    install git

---
+++ git-download.bat
@@ -0,0 +1,11 @@
+@echo off
+set currpath=%~dp0
+if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
+cd /d "%currpath%"
+for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
+
+set ver=2.33.1
+set fn=Git-%ver%-64-bit.exe
+set url=https://github.com/git-for-windows/git/releases/download/v%ver%.windows.1/%fn%
+
+call utils\curl-dl.bat "%url%" %fn%
---
+++ git-install.bat
@@ -0,0 +1,10 @@
+@echo off
+set currpath=%~dp0
+if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
+cd /d "%currpath%"
+for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
+
+set ver=2.33.1
+set fn=Git-%ver%-64-bit.exe
+
+%archivespath%\%fn% /VERYSILENT
