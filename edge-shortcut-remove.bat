bd59f0caf8a7e63887e10b49fb572630e9fc0073
sammtcbn <sammtcbn@gmail.com>
2021-11-16 10:19:09 +0800

    remove shortcut for edge browser

---
+++ edge-shortcut-remove.bat
@@ -0,0 +1,9 @@
+@echo off
+set currpath=%~dp0
+if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
+cd /d "%currpath%"
+for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
+
+set shortcut-filename=Microsoft Edge.lnk
+
+call utils\desktop-file-remove.bat %shortcut-filename%
