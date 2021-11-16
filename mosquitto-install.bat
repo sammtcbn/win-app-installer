2348c9d94b3b4d3817fa66803e9c289e0baa82eb
sammtcbn <sammtcbn@gmail.com>
2021-11-16 16:24:57 +0800

    mosquitto change to 2.0.13

--- mosquitto-install.bat
+++ mosquitto-install.bat
@@ -4,7 +4,7 @@ if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
 cd /d "%currpath%"
 for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
 
-set ver=2.0.12
+set ver=2.0.13
 set fn=mosquitto-%ver%-install-windows-x64.exe
 
 %archivespath%\%fn% /S
