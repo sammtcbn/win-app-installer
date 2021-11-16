d0f68790698cdf8388f4aeda9c90df9cc9a601dd
sammtcbn <sammtcbn@gmail.com>
2021-11-16 16:42:29 +0800

    script for install nodejs package

---
+++ nodejs-pkg-ins.bat
@@ -0,0 +1,16 @@
+@echo off
+set currpath=%~dp0
+if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
+cd /d "%currpath%"
+for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
+
+if "%~1"=="" goto END
+
+WHERE npm
+if /I %ERRORLEVEL% NEQ 0 (
+    goto END
+)
+
+npm install -g %*
+
+:END
