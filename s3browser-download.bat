b4b964333f4d92776116aa3258dce8364b0de6f5
sammtcbn <sammtcbn@gmail.com>
2021-12-24 17:20:33 +0800

    s3brwoser change to 10.3.1

--- s3browser-download.bat
+++ s3browser-download.bat
@@ -4,7 +4,7 @@ if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
 cd /d "%currpath%"
 for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
 
-set ver=10-2-3
+set ver=10-3-1
 set fn=s3browser-%ver%.exe
 set url=https://s3browser.com/download/%fn%
 
