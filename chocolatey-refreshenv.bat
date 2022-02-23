a7e54c6003d4ad2ebc4b52fff3e0087cd4d7dfa3
sammtcbn <sammtcbn@gmail.com>
2022-02-23 18:17:05 +0800

    minor update

--- chocolatey-refreshenv.bat
+++ chocolatey-refreshenv.bat
@@ -2,7 +2,6 @@
 set currpath=%~dp0
 if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
 cd /d "%currpath%"
-for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
 
 WHERE refreshenv
 if /I %ERRORLEVEL% NEQ 0 (
