06376422d5291295e9a01ca344925cfbf6c3387b
sammtcbn <sammtcbn@gmail.com>
2022-02-23 18:35:46 +0800

    minor update

--- python-pkg-ins.bat
+++ python-pkg-ins.bat
@@ -1,8 +1,4 @@
 @echo off
-set currpath=%~dp0
-if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
-cd /d "%currpath%"
-for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
 
 if "%~1"=="" goto END
 
