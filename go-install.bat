f434bf367cb1a3ef057c93e43dcc6e5e84f30a5d
sammtcbn <sammtcbn@gmail.com>
2022-06-21 14:48:03 +0800

    go change to v1.18.3

--- go-install.bat
+++ go-install.bat
@@ -4,7 +4,7 @@ if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
 cd /d "%currpath%"
 for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
 
-set ver=1.17.3
+set ver=1.18.3
 set fn=go%ver%.windows-amd64.msi
 
 %archivespath%\%fn% /quiet /qn /norestart
