db3372212daea846a6b15190c22af6440404864a
sammtcbn <sammtcbn@gmail.com>
2022-07-13 16:19:28 +0800

    git change to 2.37.1

--- git-install.bat
+++ git-install.bat
@@ -4,7 +4,7 @@ if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
 cd /d "%currpath%"
 for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
 
-set ver=2.34.0
+set ver=2.37.1
 set fn=Git-%ver%-64-bit.exe
 
 %archivespath%\%fn% /VERYSILENT
