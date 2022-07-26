067a162c6b44e0d52eaee7d5f2bd18c3798468f1
sammtcbn <sammtcbn@gmail.com>
2022-07-26 13:48:44 +0800

    7-Zip change to v22.01

--- 7zip-install.bat
+++ 7zip-install.bat
@@ -4,7 +4,7 @@ if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
 cd /d "%currpath%"
 for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
 
-set ver=1900
+set ver=2201
 set fn=7z%ver%-x64.exe
 
 %archivespath%\%fn% /S
