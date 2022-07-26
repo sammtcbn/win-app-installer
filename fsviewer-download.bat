1e78be6f7e118d9acef527375a788ebf1f708d0d
sammtcbn <sammtcbn@gmail.com>
2022-07-26 13:43:36 +0800

    FastStone Image Viewer change to v7.6

--- fsviewer-download.bat
+++ fsviewer-download.bat
@@ -4,8 +4,8 @@ if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
 cd /d "%currpath%"
 for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
 
-set ver=75
-set fn=FSViewerSetup75.exe
+set ver=76
+set fn=FSViewerSetup%ver%.exe
 set url=https://www.faststonesoft.net/DN/%fn%
 
 call utils\curl-dl.bat "%url%" %fn%
