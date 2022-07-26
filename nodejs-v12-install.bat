449499f12d29dc77f02f1c0e721c47f7cd758ba5
sammtcbn <sammtcbn@gmail.com>
2022-07-26 13:59:43 +0800

    nodejs v12 change to v12.22.12

--- nodejs-v12-install.bat
+++ nodejs-v12-install.bat
@@ -4,7 +4,7 @@ if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
 cd /d "%currpath%"
 for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
 
-set ver=12.22.7
+set ver=12.22.12
 set fn=node-v%ver%-x64.msi
 
 %archivespath%\%fn% /quiet
