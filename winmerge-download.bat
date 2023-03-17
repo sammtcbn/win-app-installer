8356ddf33541a236c8b67b97d1757a6a3478ee5c
sammtcbn <sammtcbn@gmail.com>
2023-03-17 20:38:12 +0800

    WinMerge change to v2.16.26

--- winmerge-download.bat
+++ winmerge-download.bat
@@ -4,7 +4,7 @@ if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
 cd /d "%currpath%"
 for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
 
-set ver=2.16.20
+set ver=2.16.26
 set fn=WinMerge-%ver%-x64-Setup.exe
 set url=https://github.com/WinMerge/winmerge/releases/download/v%ver%/%fn%
 
