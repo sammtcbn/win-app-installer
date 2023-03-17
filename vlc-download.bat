03450e536ea25e2b4b08a37515f7994981e6dee8
sammtcbn <sammtcbn@gmail.com>
2023-03-17 21:46:54 +0800

    VLC change to v3.0.18

--- vlc-download.bat
+++ vlc-download.bat
@@ -4,7 +4,7 @@ if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
 cd /d "%currpath%"
 for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
 
-set ver=3.0.16
+set ver=3.0.18
 set fn=vlc-%ver%-win64.exe
 set url=https://get.videolan.org/vlc/%ver%/win64/%fn%
 
