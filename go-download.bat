5c3a91a1f92ffa8cfd8865ea7720654d88abcbcc
sammtcbn <sammtcbn@gmail.com>
2023-03-20 20:36:35 +0800

    change download path for golang

--- go-download.bat
+++ go-download.bat
@@ -6,6 +6,6 @@ for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
 
 set ver=1.20.2
 set fn=go%ver%.windows-amd64.msi
-set url=https://golang.org/dl/%fn%
+set url=https://go.dev/dl/%fn%
 
 call utils\curl-dl.bat "%url%" %fn%
