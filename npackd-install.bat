602c2b600747cf854a5509b284f9e138b0b177e5
sammtcbn <sammtcbn@gmail.com>
2023-03-21 12:49:00 +0800

    install Npackd

---
+++ npackd-install.bat
@@ -0,0 +1,10 @@
+@echo off
+set currpath=%~dp0
+if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
+cd /d "%currpath%"
+for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
+
+set ver=1.26.9
+set fn=Npackd64-%ver%.msi
+
+%archivespath%\%fn% /quiet /qn /norestart
