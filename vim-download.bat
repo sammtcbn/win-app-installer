a9e125cab5251a43985c6b65bca1f0a7bec21b8f
sammtcbn <sammtcbn@gmail.com>
2023-03-21 16:11:27 +0800

    install vim

---
+++ vim-download.bat
@@ -0,0 +1,11 @@
+@echo off
+set currpath=%~dp0
+if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
+cd /d "%currpath%"
+for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
+
+set ver=9.0.1418
+set fn=gvim_%ver%_x64.exe
+set url=https://github.com/vim/vim-win32-installer/releases/download/v%ver%/%fn%
+
+call utils\curl-dl.bat "%url%" %fn%
---
+++ vim-install.bat
@@ -0,0 +1,10 @@
+@echo off
+set currpath=%~dp0
+if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
+cd /d "%currpath%"
+for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
+
+set ver=9.0.1418
+set fn=gvim_%ver%_x64.exe
+
+%archivespath%\%fn% /S
