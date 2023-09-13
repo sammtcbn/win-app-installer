906c68494b7bd76d69ecd0127f04438c46b12edf
sammtcbn <sammtcbn@gmail.com>
2023-09-13 10:50:08 +0800

    add XML Notepad

---
+++ xmlnotepad-download.bat
@@ -0,0 +1,11 @@
+@echo off
+set currpath=%~dp0
+if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
+cd /d "%currpath%"
+for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
+
+set ver=2.9.0.5
+set fn=XmlNotepad-%ver%-Setup.zip
+set url=https://github.com/microsoft/XmlNotepad/releases/download/%ver%/XmlNotepadSetup.zip
+
+call utils\curl-dl.bat "%url%" %fn%
---
+++ xmlnotepad-install.bat
@@ -0,0 +1,12 @@
+@echo off
+set currpath=%~dp0
+if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
+cd /d "%currpath%"
+for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
+
+set ver=2.9.0.5
+set fn=XmlNotepad-%ver%-Setup.zip
+
+call utils\zip-extract.bat %archivespath%\%fn% %tmppath%
+%tmppath%\XmlNotepadSetup.msi /quiet /qn /norestart
+del /f /q %tmppath%\*
