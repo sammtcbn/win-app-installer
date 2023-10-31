605c1e4270bac5d7c34d91b3eaf7451529ccf990
sammtcbn <sammtcbn@gmail.com>
2023-11-01 01:36:34 +0800

    winmerge change to v2.16.34

--- winmerge-install.bat
+++ winmerge-install.bat
@@ -4,7 +4,7 @@ if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
 cd /d "%currpath%"
 for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
 
-set ver=2.16.26
+set ver=2.16.34
 set fn=WinMerge-%ver%-x64-Setup.exe
 
 %archivespath%\%fn% /VERYSILENT /SP- /NORESTART
