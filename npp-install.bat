990f06047f65325c884ffc2a98971abf2c6b5844
sammtcbn <sammtcbn@gmail.com>
2023-10-31 14:30:22 +0800

    NotePad++ change to v8.5.8

--- npp-install.bat
+++ npp-install.bat
@@ -4,7 +4,7 @@ if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
 cd /d "%currpath%"
 for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
 
-set ver=8.5.7
+set ver=8.5.8
 set fn=npp.%ver%.Installer.x64.exe
 
 %archivespath%\%fn% /S
