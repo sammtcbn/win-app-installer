be0ea16501429e128c9801de3ca56898028370d9
sammtcbn <sammtcbn@gmail.com>
2023-12-29 15:39:10 +0800

    python change to v3.12.1

--- python-download.bat
+++ python-download.bat
@@ -4,7 +4,7 @@ if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
 cd /d "%currpath%"
 for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
 
-set ver=3.12.0
+set ver=3.12.1
 set fn=python-%ver%-amd64.exe
 set url=https://www.python.org/ftp/python/%ver%/%fn%
 
