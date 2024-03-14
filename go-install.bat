70343927e54ea3b477228f569e7354301e9917eb
sammtcbn <sammtcbn@gmail.com>
2024-03-14 14:19:45 +0800

    go change to v1.22.1

--- go-install.bat
+++ go-install.bat
@@ -4,7 +4,7 @@ if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
 cd /d "%currpath%"
 for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
 
-set ver=1.21.4
+set ver=1.22.1
 set fn=go%ver%.windows-amd64.msi
 
 %archivespath%\%fn% /quiet /qn /norestart
