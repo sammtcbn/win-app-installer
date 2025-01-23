16f1d6b447b3dec9e33a65a09717fe6bbf0581cc
sammtcbn <sammtcbn@gmail.com>
2025-01-23 14:44:31 +0800

    Xml Notepad change to v2.9.0.16

--- xmlnotepad-install.bat
+++ xmlnotepad-install.bat
@@ -4,7 +4,7 @@ if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
 cd /d "%currpath%"
 for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
 
-set ver=2.9.0.5
+set ver=2.9.0.16
 set fn=XmlNotepad-%ver%-Setup.zip
 
 call utils\zip-extract.bat %archivespath%\%fn% %tmppath%
