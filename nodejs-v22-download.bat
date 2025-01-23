6188728427f221749d534514d39c09d9838f68df
sammtcbn <sammtcbn@gmail.com>
2025-01-23 14:53:30 +0800

    install node.js v22

---
+++ nodejs-v22-download.bat
@@ -0,0 +1,11 @@
+@echo off
+set currpath=%~dp0
+if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
+cd /d "%currpath%"
+for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
+
+set ver=22.13.1
+set fn=node-v%ver%-x64.msi
+set url=https://nodejs.org/dist/v%ver%/%fn%
+
+call utils\curl-dl.bat "%url%" %fn%
