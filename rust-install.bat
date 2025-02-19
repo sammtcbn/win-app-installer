8f37d9c4649d77e3333657dc8c2cd59e36a9e927
sammtcbn <sammtcbn@gmail.com>
2025-02-19 20:35:47 +0800

    rust change to v1.84.1

--- rust-install.bat
+++ rust-install.bat
@@ -4,7 +4,7 @@ if "%currpath:~-1%"=="\" set currpath=%currpath:~0,-1%
 cd /d "%currpath%"
 for /f "delims=" %%x in (utils\env.txt) do (set "%%x")
 
-set ver=1.56.1
+set ver=1.84.1
 set fn=rust-%ver%-x86_64-pc-windows-gnu.msi
 
 %archivespath%\%fn% /quiet /qn /norestart
